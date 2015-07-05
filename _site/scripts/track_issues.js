1bf842e7bc24cf725525ddbcef024744f56b0ef4
Vale <valetolpegin@gmail.com>
2015-07-05 07:25:30 -0400

    Updated current status page

--- _site/scripts/track_issues.js
+++ _site/scripts/track_issues.js
@@ -1,23 +1,60 @@
 $( document ).ready( function() {
-  /*var github = new Github({});
-  var issues = github.getIssues( "DarkmatterVale", "regex4dummies" );
-  console.log( issues );
+  var api = "https://api.github.com/repos/darkmattervale/regex4dummies/issues";
+  $.get(api, function (data) {
+    $.each(data, function (idx, obj) {
+      var add_progress_table = false;
+      var add_todo_table     = false;
 
-  for ( var issue in issues ) {
-    $( '#in-progress-block' ).append( "<li>" + issue.)
-  }*/
+      var progress = "Due: Continuous";
 
-  $.ajax({
-    url: "https://api.github.com/repos/darkmattervale/regex4dummies/issues",
-    dataType: "json",
-    success: function ( returnData ) {
-      //$("#in-progress-block").html(returnData[0]["object"]["sha"]);
-      alert('Load was performed.');
-      alert( JSON.parse( returnData[0].object.sha ) );
-    }
+      for ( var index = 0; index < obj.labels.length; index++ ) {
+
+        if ( JSON.stringify( "Objective" ) == JSON.stringify( obj.labels[index].name ) ) {
+          add_todo_table = true;
+        } else if ( JSON.stringify( "In-Progress" ) == JSON.stringify( obj.labels[index].name ) ) {
+          add_progress_table = true
+        }
+      }
+      try {
+        if ( obj.milestone.due_on ) {
+          progress = "Due: " + obj.milestone.due_on.substring(0, 10);
+        }
+      } catch( err ) {
+      }
+
+      if ( add_progress_table ) {
+        $("#progress_table").append( " \
+          <table class='pure-table pure-table-horizontal' width='100%' height='auto'> \
+            <tr> \
+              <td> \
+                <h4><a href='" + obj.html_url + "'>" + obj.title + "</a></h4> \
+              </td> \
+              <td></td><td></td><td></td> \
+            </tr> \
+            <tr class='pure-table-odd'> \
+              <td> \
+                <img src='" + obj.user.avatar_url + "' width='28px' height='28px'>" + "     " + obj.user.login + "</img> \
+              </td> \
+              <td></td><td></td><td style='text-align: right;'>" + progress + "</td> \
+            </tr> \
+          </table>" );
+      } else if ( add_todo_table ) {
+        $("#todo_table").append( " \
+          <table class='pure-table pure-table-horizontal' width='100%' height='auto'> \
+            <tr> \
+              <td> \
+                <h4><a href='" + obj.html_url + "'>" + obj.title + "</a></h4> \
+              </td> \
+              <td></td><td></td><td></td> \
+            </tr> \
+            <tr class='pure-table-odd'> \
+              <td> \
+                <img src='" + obj.user.avatar_url + "' width='28px' height='28px'>" + "     " + obj.user.login + "</img> \
+              </td> \
+              <td></td><td></td><td style='text-align: right;'>" + progress + "</td> \
+            </tr> \
+          </table>" );
+      }
+    });
   });
-
-  for ( var x = 0; x < 10; x++ ) {
-    $( '#in-progress-block' ).append( "<li>YOLO</li>" );
-  }
 });
