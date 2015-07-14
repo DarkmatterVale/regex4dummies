532fafb434312902deede2de0c311fe1d2dcbb57
Vale <valetolpegin@gmail.com>
2015-07-13 20:43:27 -0400

    Added base for accuracy tester class

---
+++ examples/accuracy_tester/main.py
@@ -0,0 +1,20 @@
+__author__ = 'Vale Tolpegin'
+
+"""
+
+Class information:
+
+- name: main
+- version: 1.3.4
+
+"""
+
+class main:
+    # Constructor
+    def __init__( self, *args, **kwargs ):
+        print ""
+        print "This is currently under development....Please check back soon for a working version."
+        print ""
+
+if __name__ == '__main__':
+    main()
