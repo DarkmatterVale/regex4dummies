d4a1594d5b021d30ee4a534d0060c682918e5af8
Vale <valetolpegin@gmail.com>
2015-07-13 21:11:49 -0400

    Added test structure

--- examples/accuracy_tester/main.py
+++ examples/accuracy_tester/main.py
@@ -16,5 +16,24 @@ class main:
         print "This is currently under development....Please check back soon for a working version."
         print ""
 
+        return
+
+        # Beginning of main program
+
+        # Currently implemented functions that will be tested
+        # This program performs a number of accuracy tests against the library. THey are:
+        # 1. Each individual parser is tested
+        #   a. Are the subject, verb, object, and prepositional phrases properly identified?
+        #   b. Are the applicability score and the reliability score properly calculated?
+        #   c. Are all of the patterns identified?
+        # 2. All 3 parsers in one function call
+        #   a. Are all of the patterns identified?
+        #   b. Are the subject, verb, object, and prepositional phrases properly identified?
+        #   c. Are the applicability score and the reliability score properly calculated?
+
+        # Functions to be implemented that will be tested
+        # 1. FreqDist to determine the possible topics that all fo the strings relate to
+        # 2. Topics for each pattern is correctly identified
+
 if __name__ == '__main__':
     main()
