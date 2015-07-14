35d4a951d14b2e543ff70574950d44dfa103873d
Vale <valetolpegin@gmail.com>
2015-07-13 21:44:45 -0400

    Added print statements for all basic information

--- examples/accuracy_tester/main.py
+++ examples/accuracy_tester/main.py
@@ -32,8 +32,27 @@ class main:
         #   c. Are the applicability score and the reliability score properly calculated?
 
         # Functions to be implemented that will be tested
-        # 1. FreqDist to determine the possible topics that all fo the strings relate to
+        # 1. FreqDist to determine the possible topics that all to the strings relate to
         # 2. Topics for each pattern is correctly identified
+
+        print ""
+        print "*" * 75
+        print "regex4dummies Accuracy Tester"
+        print ""
+        print "Each test is valued in the following way: "
+        print "* 100 - 90     : regex4dummies is consistently performing well and should be able to handle complex data"
+        print "* 89 - 80      : regex4dummies is performing well, but does not perform that way consistently and cannot handle very complicated data"
+        print "* 79 - 70      : regex4dummies is lacking in performance consistently and is not capable of parsing compex strings"
+        print "* 69 and below : regex4dummies is doing very poorly and is considered still under development. It should not be used in applications which will be used by users"
+        print "*" * 75
+        print ""
+        print "***Beginning Tests***"
+        # COMPLETE TESTS HERE
+        print "***Tests Completed***"
+        print ""
+        print "Overall Score : " # ADD SCORE ( Referenced from above ) HERE
+        # SHOW TEST INFORMATION HERE
+        print ""
 
 if __name__ == '__main__':
     main()
