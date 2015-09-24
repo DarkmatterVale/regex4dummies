24716043fcd08cfa1b4c3eb379a7bbded292229b
Vale <valetolpegin@gmail.com>
2015-09-24 11:44:37 -0400

    Base declarations for new functions
    - phrase_extractor
    - string_compare

---
+++ regex4dummies/phrase_extractor.py
@@ -0,0 +1,70 @@
+__author__  = 'Vale Tolpegin'
+
+"""
+
+PhraseExtractor extracts different kinds of phrases from a text
+
+Class information:
+- name: PhraseExtractor
+- version: 1.4.4
+
+"""
+
+
+class PhraseExtractor:
+
+    def __init__( self, *args, **kwargs ):
+        """ Blank constructor method """
+
+        pass
+
+
+    def extract_phrases( self, text ):
+        """
+        Returns the phrases found in a sentence,
+        in the form of a dictionary.
+        """
+
+        # Getting the different kinds of phrases
+
+        # Returing the dictionary/JSON-encoded response
+        return {}
+
+
+    def extract_noun_phrases( self, text ):
+        """
+        Returns the noun phrases found within
+        a text.
+        """
+
+        # Instantiating variables
+        noun_phrases = []
+
+        # Returning the found noun_phrases
+        return noun_phrases
+
+
+    def extract_verb_phrases( self, text ):
+        """
+        Returns the verb phrases found within
+        a text.
+        """
+
+        # Instantiating variables
+        verb_phrases = []
+
+        # Returning the found verb_phrases
+        return verb_phrases
+
+
+    def extract_prepositional_phrases( self, text ):
+        """
+        Returns the prepositional phrases found
+        within a text.
+        """
+
+        # Instantiating variables
+        prepositional_phrases = []
+
+        # Returning the found prepositional_phrases
+        return prepositional_phrases
